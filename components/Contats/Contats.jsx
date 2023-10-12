9a2628a5b12a5b19d25b793900081dcf4cd5c18f
makafreitasdev <matheus_maka@hotmail.com>
2023-10-12 12:34:46 -0300

    Footer started

--- components/Contats/Contats.jsx
+++ components/Contats/Contats.jsx
@@ -1,4 +1,5 @@
 import styles from "./Contats.module.css";
+import Image from "next/image";
 
 export default function Contats() {
   return (
@@ -7,6 +8,27 @@ export default function Contats() {
         <h1 className={styles.title}>
           <span>Gym</span> Center Training
         </h1>
+        <h2 className={styles.follow}>Siga a Gym Center Training</h2>
+        <div className={styles.socials}>
+          <Image 
+            src="/img/facebook.png"
+            width={80}
+            height={80}
+            alt="facebook"
+          />  
+          <Image 
+            src="/img/instagram.png"
+            width={80}
+            height={80}
+            alt="instagram"
+          />  
+          <Image 
+            src="/img/youtube.png"
+            width={80}
+            height={80}
+            alt="youtube"
+          />  
+        </div>
       </div>
     </div>
   );
